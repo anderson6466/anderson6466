2d044b7f9ee2dce75bcd31a1118ee396e11287bf
Lenovo <anderson6466@hotmail.com>
2019-04-18 19:13:13 -0500

    Tarea Log Example y Exponencial

---
+++ BigONotationPartial/BigONotationPartial/Exponential.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace BigONotationPartial
+{
+    class Exponential
+    {
+        static void Main(string[] args)
+        {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            Exponential exponential = new Exponential();
+            ExponentialExampleType exponentialExampleType = ExponentialExampleType.Fibonaccix;
+            switch (exponentialExampleType)
+            {
+                case ExponentialExampleType.Fibonaccix:
+                    int n = 40; //8 40 80
+                    for (int i = 1; i <= n; i++)
+                    {
+                        long   fibonacci = exponential.Fibonacci(i);
+                        Console.WriteLine("fibonacci {0} = {1}", i, fibonacci);
+                    }
+                    break;
+            }
+            Console.WriteLine("Time elapsed: {0:0.00} seconds", Math.Round(stopWatch.ElapsedMilliseconds / 1000.0, 2));
+            Console.ReadLine();
+        }
+
+        /// Complexity: O(2^N)
+        internal int Fibonacci(long n)
+        {
+           
+            if (n <= 2)
+            {
+                return 1;
+            }
+            return Fibonacci(n - 1) + Fibonacci(n - 2)
+
+                ;
+        }
+    }
+}
---
+++ BigONotationPartial/BigONotationPartial/ExponentialExampleType.cs
@@ -0,0 +1,7 @@
+﻿namespace BigONotationPartial
+{
+    enum ExponentialExampleType
+    {
+        Fibonaccix
+    }
+}
